62a844f7b26c6b5502a238996b6563907485376a
blackcater <blackcater2015@gmail.com>
2020-07-22 23:40:09 +0800

    feat: support Mulan PSL

--- scripts/plugins/wakatime.js
+++ scripts/plugins/wakatime.js
@@ -1,5 +1,11 @@
-const fs = require("fs");
-const path = require("path");
+// Copyright (c) 2020 blackcater
+// [Software Name] is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
 const _ = require("lodash");
 const { WakaTimeClient, RANGE } = require("wakatime-client");
 
